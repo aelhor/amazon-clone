99f855067776086667668559832b0f04b4c819f3
ahmedel <aelhor90@mail.com>
2022-10-14 20:13:44 +0200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import data from './data';
+import Product from './components/Product'
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">
+            amazon clone
+          </a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div>
+          <div className="row center">
+            {data.products.map((product) => (
+              <Product key={product._id} product={product}/>
+            ))}
+          </div>
+        </div>
+      </main>
+      <footer className="row center">All right reserved</footer>
+    </div>
+  );
+}
+
+export default App;
