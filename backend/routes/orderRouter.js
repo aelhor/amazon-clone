1a8127e166dd51562973151eec97af0a678fea66
ahmedel <aelhor90@mail.com>
2022-11-30 18:41:03 +0200

    adedd get orderbyId nad pay order endpoints

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -17,9 +17,9 @@ orderRouter.get('/', expressAsyncHandler(async (req, res) => {
 
 // route : GET /api/orders:orderId
 // disc : get an order by ID
-orderRouter.get('/:orderId',isAuth , expressAsyncHandler(async (req, res) => {
+orderRouter.get('/:orderId', isAuth, expressAsyncHandler(async (req, res) => {
     const order = await Order.findById(req.params.orderId)
-    res.send( order )
+    res.send(order)
 })
 );
 
@@ -28,7 +28,7 @@ orderRouter.get('/:orderId',isAuth , expressAsyncHandler(async (req, res) => {
 // disc : create an order
 orderRouter.post('/', isAuth, expressAsyncHandler(async (req, res) => {
     const data = req.body.order
-    console.log('data: ' ,data)
+    console.log('data: ', data)
     if (req.body.order.orderItems.length === 0) {
         return res.status(400).send('order is empty')
     }
@@ -49,4 +49,27 @@ orderRouter.post('/', isAuth, expressAsyncHandler(async (req, res) => {
 })
 );
 
+
+// route : PUT /api/orders
+// disc : order payment 
+orderRouter.put('/:orderId/pay', isAuth, expressAsyncHandler(async (req, res) => {
+    const orderId = req.params.orderId
+    const order = await Order.findById(orderId)
+    if (order) {
+        // update order 
+        order.isPaid = true
+        order.paidAt = Date.now()
+        order.PaymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.email_address,
+        }
+        const updatedOrder = await Order.save()
+        res.send({messsage: 'order paid', order:updatedOrder})
+    }
+    else{
+        res.status.apply(404).send({messsage:"order not found"})
+    }
+}))
 module.exports = orderRouter
