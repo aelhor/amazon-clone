c95aa58554aaad1104a7a3cfc396b45c8247aee0
Ahmed Elhor <53372116+aelhor@users.noreply.github.com>
2022-12-30 09:49:38 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,10 @@ dotenv.config();
 const cors = require('cors');
 const corsOptions = {
     //  origin: 'http://localhost:3000',
-    origin:'https://amazon-clone-f.onrender.com/',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200
+    //  origin:'https://amazon-clone-f.onrender.com/',
+    origin : false,
+    credentials : true,            //access-control-allow-credentials:true
+    optionSuccessStatus : 200
 }
 app.use(cors(corsOptions));
 
