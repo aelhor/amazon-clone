30eb9b91efa0afea80fcda666c3881bdfa37fbbf
Ahmed Elhor <53372116+aelhor@users.noreply.github.com>
2022-12-30 07:55:08 -0800

    changed origin in cors options

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ const path = require('path')
 dotenv.config();
 const cors = require('cors');
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    //  origin: 'http://localhost:3000',
+    origin:'https://amazon-clone-f.onrender.com/'
     credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200
 }
